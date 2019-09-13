18695b4c2801bf164970602d2c3cf66c1003a482
MatBitt <bittenmat@gmail.com>
2019-09-13 12:55:01 -0300

    Mudanca do req 1

--- main.py
+++ main.py
@@ -3,49 +3,66 @@ import numpy as np
 import cv2
 from camera_calibration import calibration
 from camera_calibration import correct_distortion
+from math import sqrt
 
 coord1 = (0,0)
 coord2 = (0,0)
 firstclick = 0
 
 def Draw_line(event,x,y,flags,param):
-    global coord1, coord2, firstclick
- 
-    if (event==cv2.EVENT_LBUTTONDOWN) & (firstclick==0):
-        coord1 = (x,y)
-        firstclick = 1
-    elif (event == cv2.EVENT_LBUTTONDOWN) & (firstclick==1):
-        coord2 = (x,y)
+    global contador
+    global nova_imagem
+    global pixel_inicial
+    global pixel_final
+    global aux_x
+    global aux_y
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if contador == 0:
+            aux_x = x
+            aux_y = y
+            contador = 1
+        elif contador == 1:
+            nova_imagem = 1;
+            pixel_inicial = (aux_x, aux_y)
+            pixel_final = (x,y)
+            contador = 0
+            dist = sqrt((x-aux_x)**2 + (y-aux_y)**2)
+            print(dist)
 
-    
+
 
 if(str(sys.argv[1])) == '-r1':
-    img = cv2.imread(str(sys.argv[2]))
-    print("Faca os cliques e, em seguida, pressione ESC...")
-    cv2.imshow("imagem", img)
-    cv2.setMouseCallback('imagem',Draw_line)
+    contador = 0
+    nova_imagem = 0
+    video = cv2.VideoCapture(0)
+    flag, frame = video.read()
 
-    k = cv2.waitKey(0)
-    
-    if k == 27 & firstclick==1:
-        cv2.destroyAllWindows()
-    clone = img.copy()
-    cv2.line(clone, coord1, coord2, (0,0,255), 1)
-    cv2.imshow("Linha", clone)
+    if (not (flag)):
+        exit("Error while reading the video, try again.")
 
-    distancia = np.subtract(coord1, coord2)
-    distancia = np.power(distancia, 2)
-    distancia = np.sum(distancia)
-    distancia = np.sqrt(distancia)
+    cv2.namedWindow('Webcam')
+    cv2.setMouseCallback('Webcam', Draw_line)
 
-    print(distancia)
+    while(True):
+        flag, frame = video.read()
 
-    k = cv2.waitKey()
-    if k == 27:
-        cv2.destroyAllWindows()
-    
+        if (not (flag)):
+            exit("Error while reading the video, try again.")
+
+        cv2.imshow('Webcam', frame)
+
+        if(nova_imagem):
+            resultado = cv2.line(frame, pixel_inicial, pixel_final, (0,255,0), 2, 8)
+            cv2.namedWindow('Resultado')
+            cv2.imshow('Resultado', resultado)
+
+        if cv2.waitKey(25) == 27:
+            cv2.destroyWindow('Webcam')
+            cv2.destroyWindow('Resultado')
+            break
+    video.release()
+
 if (str(sys.argv[1]) == '-r2'):
     cam = cv2.VideoCapture(0)
     mtx, dist = calibration(cam, 28, 8, 6, 5, 5)
     correct_distortion(cam, mtx, dist)
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                
